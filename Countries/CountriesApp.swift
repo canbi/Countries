8e5ae0568d303bfa3ced82e126bc2bb659f06a17
Can Bi <bica@mef.edu.tr>
2022-08-09 11:41:42 +0300

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Can Bi on 9.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
